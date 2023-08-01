2dd55a471920be9da91a8d0495e2e53ec23545b4
V_Kovshov <ssevenn41@gmail.com>
2023-08-01 23:47:31 +0300

    changed admin.model "OrderTGAdmin"

--- bot/admin.py
+++ bot/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from bot.models import Shoes, SizeQuantity
+from bot.models import Shoes, SizeQuantity, UserTG, OrderTG
 
 
 class SizeInline(admin.TabularInline):
@@ -25,3 +25,42 @@ class ShoesAdmin(admin.ModelAdmin):
 			return mark_safe(f"<img src='{object.image.url}' width=100>")
 
 	get_html_photo.short_description = 'Фото моделі'
+
+
+@admin.register(UserTG)
+class UserTGAdmin(admin.ModelAdmin):
+	list_display = ('name', 'username', 'balance')
+	fields = ('tg_id', 'name', 'username', 'phone', 'bank_card', 'balance')
+	readonly_fields = ('tg_id', )
+	search_fields = ('name', 'username', 'phone')
+	ordering = ('name', )
+
+
+@admin.register(OrderTG)
+class OrderTGAdmin(admin.ModelAdmin):
+	list_display = ('date', 'user_id', 'user_instagram', 'shoes_model', 'balance', 'invoice')
+	fields = ('user_id', 'user_instagram', 'shoes_model', 'shoes_size', 'client_name', 'client_phone', 'data', 'postpayment', 'screen_payment', 'balance', 'invoice')
+	search_fields = ('client_name', 'invoice', 'data')
+	ordering = ('date', )
+
+	def get_list_display(self, request):
+		list_display = super().get_list_display(request)
+		user_id = list_display.index('user_id')
+		user_instagram = list_display.index('user_instagram')
+		if request.user:
+			list_display = tuple(i for i in list_display if list_display.index(i) != user_id)
+		else:
+			list_display = tuple(i for i in list_display if list_display.index(i) != user_instagram)
+		return list_display
+
+	def get_fields(self, request, obj=None):
+		fields = super().get_fields(request)
+		user_id = fields.index('user_id')
+		user_instagram = fields.index('user_instagram')
+		screen_payment = fields.index('screen_payment')
+		shoes_size = fields.index('shoes_size')
+		if request.user:
+			fields = tuple(i for i in fields if fields.index(i) not in [user_id, screen_payment])
+		else:
+			fields = tuple(i for i in fields if fields.index(i) != [user_instagram, shoes_size])
+		return fields
