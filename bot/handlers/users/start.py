0fe578bb38e08bf821f2b06a751c26f2d9ed71d1
V_Kovshov <ssevenn41@gmail.com>
2023-08-09 00:03:31 +0300

    changes start registration

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -25,12 +25,12 @@ async def get_start(msg: types.Message, bot: Bot) -> None:
 
 
 @router.message(F.text == 'Оформити замовлення')
-async def get_shoes(msg: types.Message, bot: Bot):
+async def command_registration(msg: types.Message, bot: Bot):
 	user_id = msg.from_user.id
 	user_in_db = await check_user_in_db(user_id=user_id)
-	if user_in_db > 0:
-		pass
+	if user_in_db:
+		await msg.answer(f"Ви вже зареєстровані", reply_markup=reply.start_keyboard())
 	else:
 		user_msg = 'Для початку давай зареєструємо твій обліковий запис\n\n' \
-					'Щоб почати реєстрацію - натисніть\n/registration'
+				'Щоб почати реєстрацію - натисніть\n/registration'
 		await msg.answer(user_msg)
