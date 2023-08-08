17ea7437a7e990e99daa8f7f783f73e42a990f64
V_Kovshov <ssevenn41@gmail.com>
2023-08-08 23:57:15 +0300

    created user in DB

--- bot/utils/db.py
+++ bot/utils/db.py
@@ -1,11 +1,31 @@
 from asgiref.sync import sync_to_async
 from bot.models import *
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(filename)s -> [LINE:%(lineno)d] -> %(levelname)-8s [%(asctime)s] -> %(message)s',
+                    filename='bot/logging.log',
+                    filemode='w')
 
 
 @sync_to_async
-def get_size(shoes) -> list:
-    size_quantity = SizeQuantity.objects.filter(shoes=shoes)
-    size_list = []
-    for size in size_quantity:
-        size_list.append(size)
-    return size_list
+def check_user_in_db(user_id):
+    try:
+        user = UserTG.objects.get(tg_id=user_id)
+        logging.info('Получили юзера')
+        return True
+    except:
+        logging.info('Нет такого юзера')
+        return False
+
+
+@sync_to_async
+def registration_user(context_data) -> None:
+    tg_id = context_data.get('tg_id')
+    name = context_data.get('name')
+    username = context_data.get('username')
+    phone = context_data.get('phone')
+    bank_card = context_data.get('bank_card')
+    balance = context_data.get('balance')
+
+    UserTG.objects.create(tg_id=tg_id, name=name, username=username, phone=phone, bank_card=bank_card, balance=balance)
