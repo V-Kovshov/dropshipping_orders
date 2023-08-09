54813417365b2d06dbaa4ba8ba26bb2d093af3bb
V_Kovshov <ssevenn41@gmail.com>
2023-08-09 23:27:50 +0300

    added emoji for bot text

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -12,7 +12,6 @@ import logging
 
 from aiogram import types, Bot, Router, F
 from aiogram.filters import Command, CommandStart
-from asgiref.sync import sync_to_async
 
 
 router = Router()
@@ -20,17 +19,17 @@ router = Router()
 
 @router.message(CommandStart())
 async def get_start(msg: types.Message, bot: Bot) -> None:
-	await msg.answer(f"Вітаємо вас в боті &#127801<b>Roza Shoes</b>&#127801!\r\nОберіть потрібний потрібний вам пункт:",
+	await msg.answer(f"Вітаємо вас в боті &#127801<b>Roza Shoes</b>&#127801\r\n\nОберіть потрібний потрібний вам пункт:",
 					reply_markup=reply.start_keyboard())
 
 
-@router.message(F.text == 'Оформити замовлення')
+@router.message(F.text == '🛒Оформити замовлення')
 async def command_registration(msg: types.Message, bot: Bot):
 	user_id = msg.from_user.id
 	user_in_db = await check_user_in_db(user_id=user_id)
 	if user_in_db:
-		await msg.answer(f"Ви вже зареєстровані", reply_markup=reply.start_keyboard())
+		await msg.answer(f"Ви вже зареєстровані🧐", reply_markup=reply.start_keyboard())
 	else:
-		user_msg = 'Для початку давай зареєструємо твій обліковий запис\n\n' \
-				'Щоб почати реєстрацію - натисніть\n/registration'
+		user_msg = 'Для початку давай зареєструємо твій обліковий запис.\n\n' \
+				'Щоб почати реєстрацію - натисніть\n<u><b>/registration</b></u>'
 		await msg.answer(user_msg)
