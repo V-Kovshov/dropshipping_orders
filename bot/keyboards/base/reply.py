9494ce56eb6679d314afce56e8117c32fc400773
V_Kovshov <ssevenn41@gmail.com>
2023-08-09 23:28:16 +0300

    added emoji for bot text

--- bot/keyboards/base/reply.py
+++ bot/keyboards/base/reply.py
@@ -4,18 +4,18 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 def start_keyboard():
     kb_builder = ReplyKeyboardBuilder()
 
-    kb_builder.button(text='Кабінет')
-    kb_builder.button(text='Оформити замовлення')
-    kb_builder.button(text='Підтримка')
+    kb_builder.button(text='👤Кабінет')
+    kb_builder.button(text='🛒Оформити замовлення')
+    kb_builder.button(text='🤝Підтримка')
     kb_builder.adjust(1, 2)
 
-    return kb_builder.as_markup(resize_keyboard=True, input_field_placeholder='Просто натисни на одну з кнопок', one_time_keyboard=True)
+    return kb_builder.as_markup(resize_keyboard=True, input_field_placeholder='Оберіть один із варіантів', one_time_keyboard=True)
 
 
 def get_phone_keyboard():
     kb_builder = ReplyKeyboardBuilder()
 
-    kb_builder.button(text='Відправити свій номер телефону', request_contact=True)
+    kb_builder.button(text='☎️Відправити свій номер телефону', request_contact=True)
 
     return kb_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
 
@@ -23,9 +23,9 @@ def get_phone_keyboard():
 def confirmation_data():
     kb_builder = ReplyKeyboardBuilder()
 
-    kb_builder.button(text='Все вірно, завершити реєстрацію')
-    kb_builder.button(text='Є помилкові дані, почнемо заново')
-    kb_builder.button(text='Відмінити реєстрацію')
+    kb_builder.button(text='✅Все вірно, підтверджую реєстрацію')
+    kb_builder.button(text='📛Є помилкові дані, почнемо заново')
+    kb_builder.button(text='❌Відмінити реєстрацію')
 
     kb_builder.adjust(1, 1, 1)
 
