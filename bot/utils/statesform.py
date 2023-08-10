5eb42afbe2b5f51ada0f02405aba54097285d265
V_Kovshov <ssevenn41@gmail.com>
2023-08-10 21:38:19 +0300

    created order's FSM

--- bot/utils/statesform.py
+++ bot/utils/statesform.py
@@ -6,3 +6,14 @@ class FSMRegisterForm(StatesGroup):
 	GET_PHONE = State()
 	GET_CARD = State()
 	FINISH_REGISTER = State()
+
+
+class FSMCreateOrder(StatesGroup):
+	SHOES_MODEL = State()
+	SHOES_SIZE = State()
+	CLIENT_NAME = State()
+	CLIENT_PHONE = State()
+	OTHER_DATA = State()
+	POSTPAYMENT = State()
+	BALANCE_PAY = State()
+	SCREEN_PAYMENT = State()
