029ea9d46674fe14bb828e13c89866b07294c988
V_Kovshov <ssevenn41@gmail.com>
2023-08-10 20:36:55 +0300

    added typehinting

--- bot/handlers/users/create_order.py
+++ bot/handlers/users/create_order.py
@@ -4,17 +4,16 @@ from aiogram.types import Message
 from bot.keyboards.base import reply
 from bot.utils.db import check_user_in_db
 
-
 router = Router()
 
 
 @router.message(F.text == '🛒Оформити замовлення')
-async def place_order(msg: Message, bot: Bot):
-	user_id = msg.from_user.id
-	user_in_db = await check_user_in_db(user_id=user_id)
-	if user_in_db:
-		await msg.answer(f"Ви вже зареєстровані🧐", reply_markup=reply.start_keyboard())
-	else:
-		user_msg = 'Для початку давай зареєструємо твій обліковий запис.\n\n' \
-				'Щоб почати реєстрацію - натисніть\n<u><b>/registration</b></u>'
-		await msg.answer(user_msg)
+async def place_order(msg: Message, bot: Bot) -> None:
+    user_id = msg.from_user.id
+    user_in_db = await check_user_in_db(user_id=user_id)
+    if user_in_db:
+        await msg.answer(f"Ви вже зареєстровані🧐", reply_markup=reply.start_keyboard())
+    else:
+        user_msg = 'Для початку давай зареєструємо твій обліковий запис.\n\n' \
+                   'Щоб почати реєстрацію - натисніть\n<u><b>/registration</b></u>'
+        await msg.answer(user_msg)
