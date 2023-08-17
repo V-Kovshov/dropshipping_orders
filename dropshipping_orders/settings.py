1962bd3f1bf6dc320ae30adeb57ddcafa34e0e0f
V_Kovshov <ssevenn41@gmail.com>
2023-08-17 20:56:26 +0300

    moved the folder to "bot" folder

--- dropshipping_orders/settings.py
+++ dropshipping_orders/settings.py
@@ -132,8 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = []
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'bot/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'bot/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
