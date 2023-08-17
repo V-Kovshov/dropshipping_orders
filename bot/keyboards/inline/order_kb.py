dfddf28320496fb38deb9df77510dd1a71a4ce39
V_Kovshov <ssevenn41@gmail.com>
2023-08-17 20:36:12 +0300

    modified function "get inline size"

--- bot/keyboards/inline/order_kb.py
+++ bot/keyboards/inline/order_kb.py
@@ -1,11 +1,24 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from asgiref.sync import async_to_sync
+
+from bot.utils.db import Order
+
+order = Order()
 
 
 def get_inline_shoes(*args):
 	models_lst = list(*args)
 	kb = InlineKeyboardBuilder()
 	for model in models_lst:
-		kb.add(InlineKeyboardButton(text=str(model), callback_data=model.slug))
+		kb.add(InlineKeyboardButton(text=str(model), callback_data=str(model.id)))
+	kb.adjust(1)
+	return kb.as_markup()
+
+
+def get_inline_size(sizes):
+	kb = InlineKeyboardBuilder()
+	for size in sizes:
+		kb.add(InlineKeyboardButton(text=str(size), callback_data=str(size.id)))
 	kb.adjust(1)
 	return kb.as_markup()
