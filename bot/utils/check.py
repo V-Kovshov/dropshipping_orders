db7502a3e3291c412e7bbeca8bc0c01583877781
V_Kovshov <ssevenn41@gmail.com>
2023-08-25 22:46:28 +0300

    added check shoes price

--- bot/utils/check.py
+++ bot/utils/check.py
@@ -1,4 +1,5 @@
 from asgiref.sync import sync_to_async
+from bot.models import *
 
 
 @sync_to_async
@@ -19,3 +20,13 @@ def check_pay_sum(amount: str) -> bool:
 				return False
 		return True
 	return False
+
+
+@sync_to_async
+def check_shoes_price(model_id: int, price: str) -> bool:
+	price = int(price)
+	shoes = Shoes.objects.get(id=model_id)
+	if shoes.price_opt > price:
+		return True
+	return False
+
