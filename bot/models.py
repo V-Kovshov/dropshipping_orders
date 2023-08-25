57a09b6d760f85aedc3a47114eef7bd2c46e3c22
V_Kovshov <ssevenn41@gmail.com>
2023-08-25 18:04:29 +0300

    added new model for order from Instaram

--- bot/models.py
+++ bot/models.py
@@ -31,27 +31,57 @@ class OrderTG(models.Model):
     ]
 
     user_id = models.ForeignKey(UserTG, on_delete=models.DO_NOTHING, verbose_name='Здав замовлення', null=True, blank=True)
-    user_instagram = models.CharField(max_length=64, verbose_name='Нік в інстаграмі', null=True, blank=True)
     shoes_model = models.ForeignKey(to='Shoes', on_delete=models.DO_NOTHING, verbose_name='Модель замовлення')
     shoes_size = models.CharField(max_length=2, choices=SIZES_LIST, verbose_name='Розмір', null=True, blank=True)
     date = models.DateField(auto_now_add=True, verbose_name='Дата замовлення')
     client_name = models.CharField(max_length=64, verbose_name='ПІБ клієнта')
     client_phone = models.CharField(max_length=13, verbose_name='Телефон клієнта')
-    data = models.TextField(verbose_name='Інші дані для відправки')
+    other_data = models.TextField(verbose_name='Інші дані для відправки')
     postpayment = models.IntegerField(verbose_name='Накладний платіж', default=0)
     balance_pay = models.IntegerField(verbose_name='Аванс с балансу', default=0)
-    screen_payment = models.ImageField(upload_to=f'screen_payment/{user_id}', verbose_name='Скрін оплати')
+    screen_payment = models.ImageField(upload_to=f'screen_payment/{user_id}', verbose_name='Скрін оплати', null=True, blank=True)
     balance = models.FloatField(verbose_name='Баланс с замовлення', default=0.0)
     invoice = models.CharField(max_length=14, verbose_name='ТТН', null=True, blank=True)
+    issued = models.BooleanField(verbose_name='Замовлення видане', default=False)
 
     def __str__(self):
-        if self.user_id is not None:
-            return f"Замовлення від {self.user_id}"
-        return f'Замовлення від {self.user_instagram}'
+        return f"Замовлення від {self.user_id}"
 
     class Meta:
-        verbose_name = 'замовлення'
-        verbose_name_plural = 'Усі замовлення'
+        verbose_name = 'замовлення з ТГ'
+        verbose_name_plural = 'Усі замовлення з ТГ'
+
+
+class OrderInstagram(models.Model):
+    SIZES_LIST = [
+        ('34', 34),
+        ('35', 35),
+        ('36', 36),
+        ('37', 37),
+        ('38', 38),
+        ('39', 39),
+        ('40', 40),
+        ('41', 41),
+    ]
+
+    user_instagram = models.CharField(max_length=64, verbose_name='Нік в інстаграмі')
+    shoes_model = models.ForeignKey(to='Shoes', on_delete=models.DO_NOTHING, verbose_name='Модель замовлення')
+    shoes_size = models.CharField(max_length=2, choices=SIZES_LIST, verbose_name='Розмір')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата замовлення')
+    client_name = models.CharField(max_length=64, verbose_name='ПІБ клієнта')
+    client_phone = models.CharField(max_length=13, verbose_name='Телефон клієнта')
+    other_data = models.TextField(verbose_name='Інші дані для відправки')
+    advance = models.IntegerField(verbose_name='Аванс', default=0)
+    postpayment = models.IntegerField(verbose_name='Накладний платіж', default=0)
+    invoice = models.CharField(max_length=14, verbose_name='ТТН', null=True, blank=True)
+    issued = models.BooleanField(verbose_name='Замовлення видане', default=False)
+
+    def __str__(self):
+        return f"Замовлення від {self.user_instagram}"
+
+    class Meta:
+        verbose_name = 'замовлення з Instagram'
+        verbose_name_plural = 'Усі замовлення з Instagram'
 
 
 class Shoes(models.Model):
