1ddc7241c154801b043e72fea4835445b604ab31
V_Kovshov <ssevenn41@gmail.com>
2023-11-08 22:42:56 +0200

    added help user handler and created back btn

--- bot/handlers/users/personal_cabinet.py
+++ bot/handlers/users/personal_cabinet.py
@@ -3,7 +3,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 
 from bot.utils.db import check_orders_balance, get_users_orders, get_orders_by_client_surname
-from bot.utils.statesform import FSMSearchOrder
+from bot.utils.statesform import FSMSearchOrderFromProfile
 from bot.keyboards.base import reply
 from bot.keyboards.inline import user_profile_kb
 
@@ -11,32 +11,49 @@ from bot.keyboards.inline import user_profile_kb
 router = Router()
 
 
-@router.message(F.text == '👤Кабінет')
+@router.message(F.text == 'Кабінет🏛')
 async def private_cabinet(msg: Message) -> None:
-	await msg.answer(text='Користуйся кнопками👇🏼', reply_markup=reply.kb_profile())
+	await msg.answer(text='Користуйся кнопками👇🏼', reply_markup=reply.profile_kb())
 
 
-@router.message(F.text == 'Мій баланс')
+@router.message(F.text == 'Мій баланс💰')
 async def my_balance(msg: Message) -> None:
 	balance = await check_orders_balance(msg.from_user.id)
-	await msg.answer(f'Ваш баланс: {balance}грн')
+	await msg.answer(f'Ваш баланс: {balance}грн', reply_markup=reply.back_to_profile_kb())
 
 
-@router.message(F.text == 'Мої замовлення')
+@router.message(F.text == 'Мої замовлення🛍')
 async def my_orders(msg: Message) -> None:
 	user_id = msg.from_user.id
 	all_orders = await get_users_orders(user_id)
 	await msg.answer(text='Усі замовлення:', reply_markup=user_profile_kb.all_orders_kb(all_orders))
 
 
-@router.message(F.text == 'Пошук замовлення')
+@router.message(F.text == 'Пошук замовлення🔍')
 async def search_order(msg: Message, state: FSMContext) -> None:
 	await msg.answer(text='Введіть прізвище клієнта:')
-	await state.set_state(FSMSearchOrder.SURNAME_CLIENT)
+	await state.set_state(FSMSearchOrderFromProfile.SURNAME_CLIENT)
 
 
-@router.message(FSMSearchOrder.SURNAME_CLIENT)
+@router.message(FSMSearchOrderFromProfile.SURNAME_CLIENT)
 async def get_surname_client(msg: Message, state: FSMContext) -> None:
 	user_id = msg.from_user.id
 	surname_client = msg.text
 	found_orders = await get_orders_by_client_surname(user_id, surname_client)
+	await msg.answer(
+		text='Замовлення за вашим запитом:🕵🏼',
+		reply_markup=user_profile_kb.found_orders_kb(found_orders))
+	await state.clear()
+
+
+@router.message(F.text == 'Допомога⚙️')
+async def help_user(msg: Message) -> None:
+	await msg.answer(f'В нас самих іноді виникають питання до самих себе🤷🏼‍♀️\n'
+					f'Але ви завжди можете звернутися до\n'
+					f'нашого менеджера: @test_name🔮',
+					reply_markup=reply.back_to_profile_kb())
+
+
+@router.message(F.text == 'Назад в кабінет🏛')
+async def back_to_profile(msg: Message):
+	await private_cabinet(msg)
