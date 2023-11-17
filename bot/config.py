2a2c7ea098ee13a0df2781c7d9ce96e3ce28d31d
V_Kovshov <ssevenn41@gmail.com>
2023-11-17 21:57:10 +0200

    added classes for database and nova post api

--- bot/config.py
+++ bot/config.py
@@ -9,8 +9,23 @@ class Bots:
 
 
 @dataclass
+class DB:
+	user: str
+	database: str
+	password: str
+
+
+@dataclass
+class NovaPost:
+	api: str
+	phone: str
+
+
+@dataclass
 class Settings:
 	bots: Bots
+	db: DB
+	post: NovaPost
 
 
 def get_settings() -> Settings:
@@ -20,10 +35,28 @@ def get_settings() -> Settings:
 	bot_token = env.str('BOT_TOKEN')
 	admins_list = env.list('ADMIN_ID')
 
-	return Settings(bots=Bots(
-		bot_token=bot_token,
-		admin_id=admins_list
-	))
+	user_db = env.str('USER_DB')
+	database_name = env.str('DATABASE_NAME')
+	password_db = env.str('PASSWORD_DB')
+
+	api_key = env.str('API_KEY_NOVA_POST')
+	phone_number = env.str('PHONE_NUMBER')
+
+	return Settings(
+		bots=Bots(
+			bot_token=bot_token,
+			admin_id=admins_list,
+		),
+		db=DB(
+			user=user_db,
+			database=database_name,
+			password=password_db
+		),
+		post=NovaPost(
+			api=api_key,
+			phone=phone_number
+		)
+	)
 
 
 settings = get_settings()
