57e94ec3ffa0b26c658913438982d12cab4ca000
V_Kovshov <ssevenn41@gmail.com>
2023-11-30 22:37:17 +0200

    some changes

--- bot/keyboards/inline/user_profile_kb.py
+++ bot/keyboards/inline/user_profile_kb.py
@@ -12,9 +12,10 @@ def all_orders_kb(arr, total_pages, page: int = 1) -> InlineKeyboardMarkup:
 	orders_lst = [*arr]
 	kb = InlineKeyboardBuilder()
 
-	cnt = 1 if page == 1 else (page * 3) - 2
+	cnt = 1 if page == 1 else (page * 10) - 9
 	for order in orders_lst:
-		kb.add(InlineKeyboardButton(text=f'{cnt}. {order.date} | {order.client_name}', callback_data=f'ord_{order.id}'))
+		date = f'{order.date.day}-{order.date.month}-{order.date.year}'
+		kb.add(InlineKeyboardButton(text=f'{cnt}. {date} | {order.client_name}', callback_data=f'ord_{order.id}'))
 		cnt += 1
 	kb.adjust(1)
 	kb.row(
@@ -22,6 +23,9 @@ def all_orders_kb(arr, total_pages, page: int = 1) -> InlineKeyboardMarkup:
 		InlineKeyboardButton(text=f'Стр.: {page}/{total_pages}', callback_data='#'),
 		InlineKeyboardButton(text='➡️', callback_data=PaginationCallbackFactory(action='next', page=page).pack()),
 		width=3
+	)
+	kb.row(
+		InlineKeyboardButton(text='Назад', callback_data='back')
 	)
 
 	return kb.as_markup()
