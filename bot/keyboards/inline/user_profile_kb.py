1291201a5333ade4d9f4f8ec666548b66d8900e8
V_Kovshov <ssevenn41@gmail.com>
2023-11-30 15:44:26 +0200

    added some changes

--- bot/keyboards/inline/user_profile_kb.py
+++ bot/keyboards/inline/user_profile_kb.py
@@ -8,24 +8,13 @@ class PaginationCallbackFactory(CallbackData, prefix='pag'):
 	page: int
 
 
-def paginator(page: int = 1):
-	kb = InlineKeyboardBuilder()
-	kb.row(
-		InlineKeyboardButton(text='⬅️', callback_data=PaginationCallbackFactory(action='prev', page=page).pack()),
-		InlineKeyboardButton(text='➡️', callback_data=PaginationCallbackFactory(action='next', page=page).pack()),
-		width=2
-	)
-
-	return kb.as_markup()
-
-
 def all_orders_kb(arr, total_pages, page: int = 1) -> InlineKeyboardMarkup:
 	orders_lst = [*arr]
 	kb = InlineKeyboardBuilder()
 
 	cnt = 1 if page == 1 else (page * 3) - 2
 	for order in orders_lst:
-		kb.add(InlineKeyboardButton(text=f'[{cnt}] {order.client_name}', callback_data=f'ord_{order.id}'))
+		kb.add(InlineKeyboardButton(text=f'{cnt}. {order.date} | {order.client_name}', callback_data=f'ord_{order.id}'))
 		cnt += 1
 	kb.adjust(1)
 	kb.row(
