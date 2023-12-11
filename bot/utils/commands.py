453f74be84134ef523a428655b2ad08dde348422
V_Kovshov <ssevenn41@gmail.com>
2023-12-11 21:32:09 +0200

    created commands for registered users

--- bot/utils/commands.py
+++ bot/utils/commands.py
@@ -2,15 +2,39 @@ from aiogram import Bot
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
 
-async def set_commands(bot: Bot):
+async def unregistered_users_commands(bot: Bot):
 	commands = [
-		BotCommand(
-			command='start',
-			description='Початок роботи з ботом'
-		),
 		BotCommand(
 			command='registration',
 			description='Зареєструватися в боті'
+		),
+		BotCommand(
+			command='check_availability',
+			description='Перевірити наявність'
+		)
+	]
+
+	await bot.set_my_commands(commands)
+
+
+async def registered_users_commands(bot: Bot):
+	await bot.delete_my_commands()
+	commands = [
+		BotCommand(
+			command='create_order',
+			description='Оформити замовлення'
+		),
+		BotCommand(
+			command='check_availability',
+			description='Перевірити наявність'
+		),
+		BotCommand(
+			command='profile',
+			description='Особистий кабінет'
+		),
+		BotCommand(
+			command='requisites',
+			description='Реквізити'
 		)
 	]
 
